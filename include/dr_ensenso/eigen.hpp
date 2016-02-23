c1765d246477a736a51f2f0bfb1997d57f0d8d87
Mihai Morariu <mihaimorariu@gmail.com>
2016-02-23 15:48:35 +0100

    Add function for converting an Ensenso API matrix to Eigen matrix.

--- include/dr_ensenso/eigen.hpp
+++ include/dr_ensenso/eigen.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <Eigen/Dense>
 #include <ensenso/nxLib.h>
+#include <cstdint>
+#include "util.hpp"
 
 namespace dr {
 
@@ -15,5 +17,18 @@ Eigen::AngleAxisd toEigenRotation(NxLibItem const & item);
 
 /// Convert an NxLibItem holding a pose or transformation to an Eigen::Isometry3d.
 Eigen::Isometry3d toEigenIsometry(NxLibItem const & item);
+
+template <std::size_t rows, std::size_t cols>
+Eigen::Matrix<double, rows, cols> toEigenMatrix(NxLibItem const & item) {
+	Eigen::Matrix<double, rows, cols> result;
+
+	for (std::size_t row = 0; row < rows; ++row) {
+		for (std::size_t col = 0; col < cols; ++col) {
+			result(row, col) = getNx<double>(item[col][row]);
+		}
+	}
+
+	return result;
+}
 
 }
