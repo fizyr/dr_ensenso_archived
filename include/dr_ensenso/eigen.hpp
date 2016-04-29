4b2ec909e90b07adb59dcce11948e9b442e99ab2
Hans Gaiser <hansg91@gmail.com>
2016-04-29 19:26:48 +0200

    Add setNx functions for Eigen datatypes.

--- include/dr_ensenso/eigen.hpp
+++ include/dr_ensenso/eigen.hpp
@@ -18,6 +18,18 @@ Eigen::AngleAxisd toEigenRotation(NxLibItem const & item);
 /// Convert an NxLibItem holding a pose or transformation to an Eigen::Isometry3d.
 Eigen::Isometry3d toEigenIsometry(NxLibItem const & item);
 
+/// Set an Eigen::Vector3d in an NxLibItem.
+void setNx(NxLibItem const & item, Eigen::Vector3d const & vector, std::string const & what = "");
+
+/// Set an Eigen::Vector3d in an NxLibItem.
+void setNx(NxLibItem const & item, Eigen::Translation3d const & translation, std::string const & what = "");
+
+/// Set an Eigen::Vector3d in an NxLibItem.
+void setNx(NxLibItem const & item, Eigen::AngleAxisd const & rotation, std::string const & what = "");
+
+/// Set an Eigen::Vector3d in an NxLibItem.
+void setNx(NxLibItem const & item, Eigen::Isometry3d const & isometry, std::string const & what = "");
+
 template <std::size_t rows, std::size_t cols>
 Eigen::Matrix<double, rows, cols> toEigenMatrix(NxLibItem const & item) {
 	Eigen::Matrix<double, rows, cols> result;
